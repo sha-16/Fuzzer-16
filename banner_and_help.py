7780da74866aeb320e82cc37ac53361e6a6aa01d
root <root@kali>
2021-11-10 18:31:15 -0300

    alguna que otra corrección

--- banner_and_help.py
+++ banner_and_help.py
@@ -16,4 +16,4 @@ def helper():
     print("\n~ Examples: ")
     print("\n\t-> fuzzer-16.py dictionary.txt http://127.0.0.1/")
     print("\t-> fuzzer-16.py dictionary.txt http://127.0.0.1:8080/")
-    print("\t-> fuzzer-16.py /usr/share/wordlist/rockyou.txt http://target-to-fuzz.com/")
+    print("\t-> fuzzer-16.py /usr/share/wordlist/rockyou.txt http://target-to-fuzz.com/\n")
